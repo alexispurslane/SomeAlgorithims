4f32395ce3397e827a4e35ff62fc8ec2ef62732b
christopherdumas <christopherdumas@me.com>
2015-11-27 07:08:37 -0800

    Some HLint setup stuff

--- map-reduce/MapReduce.hs
+++ map-reduce/MapReduce.hs
@@ -1,7 +1,6 @@
 module MapReduce where
-
 import qualified Data.Map as M
 
 mapReduce :: [(String, Int)] -> [(String, Int)]
 mapReduce = foldl addToKey []
-    where addToKey hl (k, v) = M.toList $ M.insertWith (+) k v $ M.fromList hl
+    where addToKey hl (k, v) = M.toList . M.insertWith (+) k v $ M.fromList hl
