98e0284493f36e1eda5d7000a4a17b4876c45f40
christopherdumas <christopherdumas@me.com>
2015-11-26 20:49:35 -0800

    Haskell MapReduce

--- map-reduce/MapReduce.hs
+++ map-reduce/MapReduce.hs
@@ -1,2 +1,7 @@
 module MapReduce where
 
+import qualified Data.Map as M
+
+mapReduce :: [(String, Int)] -> [(String, Int)]
+mapReduce = foldl addToKey []
+    where addToKey hl (k, v) = M.toList $ M.insertWith (+) k v $ M.fromList hl
