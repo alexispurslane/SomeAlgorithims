c0884cd26ee5670a0c960c9a4a5bdb6eaf28c28c
christopherdumas <christopherdumas@me.com>
2015-11-29 18:22:41 -0800

    Haskell version fixed

--- parallel-qsort/PQsort.hs
+++ parallel-qsort/PQsort.hs
@@ -2,10 +2,9 @@ module PQsort where
 import Control.Parallel (par, pseq)
 
 qsort []	= []
-qsort (x:xs) =  force small `par` (force large `pseq` (small ++ mid ++ large))
-    where small = qsort [y | y<-xs, y<x]
-          mid   = [y | y<-xs, y==x] ++ [x]
-          large = qsort [y | y<-xs, y>x]
+qsort (x:xs) =  force small `par` (force large `pseq` (small ++ x:large))
+    where small = qsort [y | y <- xs, y <= x]
+          large = qsort [y | y <- xs, y > x]
           force xs = go xs `pseq` ()
               where go (_:xs) = go xs
                     go [] = 1
--- parallel-qsort/PQsort.js
+++ parallel-qsort/PQsort.js
@@ -3,6 +3,5 @@ var Parallel = require('paralleljs');
 function pqsort (list) {
     var larger = list.filter((x) => list[0] < x);
     var smaller = list.filter((x) => list[0] >= x);
-
     
 }
