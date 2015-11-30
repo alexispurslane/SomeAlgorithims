796f8241d06d1fad52707b24e9849b20c31c08bd
christopherdumas <christopherdumas@me.com>
2015-11-29 18:21:45 -0800

    Haskell version

--- parallel-qsort/PQsort.hs
+++ parallel-qsort/PQsort.hs
@@ -1,7 +1,11 @@
 module PQsort where
-import Control.Parallel
+import Control.Parallel (par, pseq)
 
-qsort [] = []
-qsort (x:xs) = small `par` (large `pseq` (small ++ (x : large)))
-    where small = [y | y <- xs, y <= x]
-          large = [y | y <- xs, y > x]
+qsort []	= []
+qsort (x:xs) =  force small `par` (force large `pseq` (small ++ mid ++ large))
+    where small = qsort [y | y<-xs, y<x]
+          mid   = [y | y<-xs, y==x] ++ [x]
+          large = qsort [y | y<-xs, y>x]
+          force xs = go xs `pseq` ()
+              where go (_:xs) = go xs
+                    go [] = 1
